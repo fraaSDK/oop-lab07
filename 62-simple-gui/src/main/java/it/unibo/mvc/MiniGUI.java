3aba6f32fb190108908e47b4313f1c1030168689
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-08 12:32:21 +0100

    Implemented the basic swing GUI

--- 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
+++ 62-simple-gui/src/main/java/it/unibo/mvc/MiniGUI.java
@@ -1,8 +1,11 @@
 package it.unibo.mvc;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -29,8 +32,18 @@ public class MiniGUI {
     public MiniGUI() {
         final JPanel canvas = new JPanel();
         canvas.setLayout(new BorderLayout());
+
+        // Part 1.
+        final JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        canvas.add(panel, BorderLayout.CENTER);
         final JButton write = new JButton("Print a random number on standard output");
-        canvas.add(write, BorderLayout.CENTER);
+        panel.add(write, BorderLayout.CENTER);
+
+        // Part 2.
+        final JTextField result = new JTextField("Press the button below to generate a new number.");
+        canvas.add(result, BorderLayout.NORTH);
+        
         frame.setContentPane(canvas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         /*
@@ -39,7 +52,10 @@ public class MiniGUI {
         write.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent e) {
-                System.out.println(randomGenerator.nextInt());
+                // Part 3.
+                final int newResult = randomGenerator.nextInt();
+                System.out.println(newResult);
+                result.setText(Integer.toString(newResult));
             }
         });
     }
