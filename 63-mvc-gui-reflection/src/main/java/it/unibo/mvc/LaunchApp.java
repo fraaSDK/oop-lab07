7dceeb504f5c9fdfa79b93c2e66e248c6d276915
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2022-11-08 18:37:57 +0100

    Adapted the controller and main to accept multiple views

--- 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
+++ 63-mvc-gui-reflection/src/main/java/it/unibo/mvc/LaunchApp.java
@@ -1,9 +1,12 @@
 package it.unibo.mvc;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+
 import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
 import it.unibo.mvc.controller.DrawNumberControllerImpl;
 import it.unibo.mvc.model.DrawNumberImpl;
-import it.unibo.mvc.view.DrawNumberSwingView;
 
 /**
  * Application entry-point.
@@ -17,15 +20,32 @@ public final class LaunchApp {
      *
      * @param args ignored
      * @throws ClassNotFoundException if the fetches class does not exist
+     * @throws SecurityException
      * @throws NoSuchMethodException if the 0-ary constructor do not exist
      * @throws InvocationTargetException if the constructor throws exceptions
      * @throws InstantiationException if the constructor throws exceptions
      * @throws IllegalAccessException in case of reflection issues
      * @throws IllegalArgumentException in case of reflection issues
      */
-    public static void main(final String... args) {
+    public static void main(final String... args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
         final var model = new DrawNumberImpl();
         final DrawNumberController app = new DrawNumberControllerImpl(model);
-        app.addView(new DrawNumberSwingView());
+        
+        for (final var viewType : List.of("CLIView", "SwingView")) {
+            try {
+                final var classes = Class.forName("it.unibo.mvc.view.DrawNumber" + viewType);
+                for (int i = 0; i < 3; i++) {
+                    try {
+                        final var currentClass = classes.getConstructor().newInstance();
+                        app.addView((DrawNumberView) currentClass);
+                    } catch (IllegalStateException e) {
+                        e.printStackTrace();
+                    }
+                }
+            } catch (ClassNotFoundException e) {
+                System.out.println("ClassNotFound");
+                continue;
+            }
+        }
     }
 }
